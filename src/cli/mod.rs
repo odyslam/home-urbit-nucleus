568f7d778e39c0957e69bb4085efeb7913817281
Odysseas Lamtzidis <odyslam@gmail.com>
2022-03-15 14:29:39 +0200

    feat: add cli options, add vergen"

---
+++ src/cli/mod.rs
@@ -0,0 +1,63 @@
+use crate::NucleusMode;
+use clap::{Parser, Subcommand};
+use std::{net::SocketAddrV4, path::PathBuf, str::FromStr};
+
+#[derive(Debug, Subcommand)]
+#[clap(about = "Perform Ethereum RPC calls from the comfort of your command line.")]
+pub enum Subcommands {
+    StartServer {
+        #[clap(
+            short = 'b',
+            help = "IP:PORT to bind the API to.",
+            env = "NUCLEUS_BIND"
+        )]
+        bind: Option<SocketAddrV4>,
+        #[clap(
+            short,
+            help = "Relative path to the configuration file",
+            env = "NUCLEUS_CONFIG_DIR"
+        )]
+        config_dir: Option<PathBuf>,
+        #[clap(
+            short,
+            help = "IP:PORT or UNIX socket for the Docker engine",
+            env = "NUCLEUS_DOCKER_DRIVER"
+        )]
+        docker_endpoint: Option<String>,
+        #[clap(
+            short,
+            help = "How Nucleus should run Urbit, 'docker' or 'process'",
+            env = "NUCLEUS_MODE"
+        )]
+        mode: Option<NucleusMode>,
+        #[clap(
+            short = 'bd',
+            help = "Relative path to the backup directory",
+            env = "NUCLEUS_BACKUP_DIR"
+        )]
+        backup_dir: Option<PathBuf>,
+        #[clap(
+            short = 'ba',
+            help = "Every how many hours a backup of Urbit should be saved. Accepts 1 to 24",
+            env = "NUCLEUS_BACKUP_ELAPSED"
+        )]
+        backup_elapsed: Option<i64>,
+    },
+}
+#[derive(Debug, Parser)]
+#[clap(name = "nucleus", version = "0.0.1")]
+pub struct Opts {
+    #[clap(subcommand)]
+    pub sub: Subcommands,
+}
+
+/// The version message for the current program, like
+/// `forge 0.1.0 (f01b232bc 2022-01-22T23:28:39.493201+00:00)`
+pub(crate) const VERSION_MESSAGE: &str = concat!(
+    env!("CARGO_PKG_VERSION"),
+    " (",
+    env!("VERGEN_GIT_SHA_SHORT"),
+    " ",
+    env!("VERGEN_BUILD_TIMESTAMP"),
+    ")"
+);
