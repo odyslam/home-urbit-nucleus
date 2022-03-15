53c3b1e85e9978409e42c62abd480b7d90a040b3
odyslam.eth <odyslam@gmail.com>
2022-03-15 17:29:38 +0200

    feat: cli (#1)

---
+++ src/cli/mod.rs
@@ -0,0 +1,75 @@
+use crate::NucleusMode;
+use clap::Parser;
+use std::{net::SocketAddrV4, path::PathBuf};
+
+#[derive(Clone, Debug, Parser)]
+#[clap(name = "nucleus", version = VERSION_MESSAGE, about = "The core of Home-Urbit")]
+pub enum Opts {
+    #[clap(name = "start", about = "start nucleus and it's API server")]
+    StartServer {
+        #[clap(
+            name = "bind",
+            short,
+            long,
+            help = "IP:PORT to bind the API to.",
+            env = "NUCLEUS_BIND"
+        )]
+        bind: Option<SocketAddrV4>,
+        #[clap(
+            name = "config-dir",
+            short,
+            long,
+            help = "Relative path to the configuration file",
+            env = "NUCLEUS_CONFIG_DIR"
+        )]
+        config_dir: Option<PathBuf>,
+        #[clap(
+            name = "docker-driver",
+            short,
+            long,
+            help = "IP:PORT or UNIX socket for the Docker engine",
+            env = "NUCLEUS_DOCKER_DRIVER"
+        )]
+        docker_endpoint: Option<String>,
+        #[clap(
+            name = "mode",
+            short,
+            long,
+            help = "How Nucleus should run Urbit, 'docker' or 'process'",
+            env = "NUCLEUS_MODE"
+        )]
+        mode: Option<NucleusMode>,
+        #[clap(
+            name = "backup-dir",
+            long,
+            help = "Relative path to the backup directory",
+            env = "NUCLEUS_BACKUP_DIR"
+        )]
+        backup_dir: Option<PathBuf>,
+        #[clap(
+            name = "backup-interval",
+            long,
+            help = "Every how many hours a backup of Urbit should be saved. Accepts 1 to 24",
+            env = "NUCLEUS_BACKUP_INTERVAL"
+        )]
+        backup_elapsed: Option<i64>,
+        #[clap(
+            name = "logging",
+            short,
+            long,
+            help = "Sets log level via RUST_LOG: debug, info, warn"
+        )]
+        logs: Option<String>,
+    },
+}
+
+/// The version message for the current program, like
+/// `nucleus 0.1.0 (f01b232bc 2022-01-22T23:28:39.493201+00:00)`
+pub(crate) const VERSION_MESSAGE: &str = concat!(
+    env!("CARGO_PKG_VERSION"),
+    " (",
+    env!("VERGEN_GIT_SHA_SHORT"),
+    " ",
+    env!("VERGEN_BUILD_TIMESTAMP"),
+    ")"
+);
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,58 @@
 use actix_files;
 use actix_web::{middleware, web, App, HttpServer};
 use api::*;
-use tracing::{info, Level};
+use std::{net::SocketAddrV4, str::FromStr};
+use tracing::{debug, info};
 use tracing_subscriber;
+
+use clap::Parser;
+use cli::Opts;
+
 mod api;
+mod cli;
 mod config;
 mod docker_driver;
 mod urbit_driver;
+mod utils;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Enable logging
-    std::env::set_var("RUST_LOG", "DEBUG");
-    tracing_subscriber::fmt::init();
-    // Main actix web app
+    let opts = Opts::parse();
+    match opts.clone() {
+        Opts::StartServer {
+            bind,
+            config_dir,
+            docker_endpoint,
+            mode,
+            backup_dir,
+            backup_elapsed,
+            logs,
+        } => {
+            std::env::set_var("RUST_LOG", logs.unwrap_or_else(|| "warn".to_string()));
+            tracing_subscriber::fmt::init();
+            debug!("{:?}", opts);
+            info!("Hello Martian, welcome to Nucleus!");
+            info!(
+                r#"
+         __
+ _(\    |@@|
+(__/\__ \--/ __
+   \___|----|  |   __
+     \ }}{{ /\ )_ / _\
+       /\__/\ \__O (__
+      (--/\--)    \__/
+      _)(  )(_
+     `---''---`
+     "#
+            );
+            start_api(bind).await
+        }
+    }
+}
+
+async fn start_api(bind_or_none: Option<SocketAddrV4>) -> std::io::Result<()> {
+    let bind = bind_or_none.unwrap_or_else(|| "127.0.0.1:6969".parse::<SocketAddrV4>().unwrap());
+    info!("Binding API server to {}", bind);
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Logger::default())
@@ -34,7 +73,25 @@ async fn main() -> std::io::Result<()> {
                     .route("/status", web::get().to(status)),
             )
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(bind)?
     .run()
     .await
+}
+
+#[derive(Debug, Clone)]
+pub enum NucleusMode {
+    Docker,
+    Process,
+}
+
+impl FromStr for NucleusMode {
+    type Err = String;
+
+    fn from_str(input: &str) -> Result<NucleusMode, Self::Err> {
+        match input {
+            "docker" => Ok(NucleusMode::Docker),
+            "process" => Ok(NucleusMode::Process),
+            _ => Err(format!("'{}' is not a valid value for NucleusMode", input)),
+        }
+    }
 }
